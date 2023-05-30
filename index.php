9a6857fdb366c2a6d04a9f3535d8108a4f8879dd
HugoVirot <hugo.virot@gmail.com>
2023-05-30 12:25:33 +0200

    structure de base

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+// j'inclus le fichier des fonctions pour pouvoir les appeler ici
+include 'functions.php';
+
+// 
+session_start();
+
+// j'inclus le head avec les balises de base + la balise head (pour ne pas répéter le code qu'il contient)
+include 'head.php';
+?>
+
+<body>
+    <?php 
+    include 'header.php';
+    ?>
+
+    <main>
+    <h1>Boutique php</h1>
+       <?php 
+       
+       ?>
+    </main>
+
+    <?php 
+    include 'footer.php';
+    ?>
+    
